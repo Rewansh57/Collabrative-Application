5a2966aa55593f96572fd23113dbe151f3ce61d9
Rewansh Choudhary <rewanshchoudhary57@gmail.com>
2025-06-11 13:52:39 +0530

    Custom port and manual config of tomcat created

--- src/main/java/org/example/projectsh/realtimechat/RealTimeChatApplication.java
+++ src/main/java/org/example/projectsh/realtimechat/RealTimeChatApplication.java
@@ -1,60 +1,68 @@
 package org.example.projectsh.realtimechat;
-
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
 
 @SpringBootApplication
 public class RealTimeChatApplication {
 
+
+
     public static void main(String[] args) {
-        SpringApplicationBuilder builder=new SpringApplicationBuilder(RealTimeChatApplication.class);
-        builder.web(WebApplicationType.SERVLET);
-        ServletWebServerApplicationContext context =
-                (ServletWebServerApplicationContext) builder.run(args);
+        Scanner sc = new Scanner(System.in);
+        SpringApplication app=new SpringApplication(RealTimeChatApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        ConfigurableApplicationContext context =app.run(args);
 
-        Scanner sc=new Scanner(System.in);
-        WebServer webServer=(context).getWebServer();
+        TomcatServletWebServerFactory factory=context.getBean(TomcatServletWebServerFactory.class);
+        int port=sc.nextInt();
+        sc.nextLine();
 
-        boolean running=true;
+        factory.setPort(port);
 
-        while(running){
-            String command=sc.nextLine().trim().toLowerCase();
+        WebServer webServer=factory.getWebServer();
 
-            switch(command){
+
+
+
+
+
+        boolean running = true;
+
+        while (running) {
+            String command = sc.nextLine().trim().toLowerCase();
+
+            switch (command) {
                 case "start":
                     webServer.start();
                     System.out.println("Web Server started");
                     break;
                 case "stop":
                     webServer.stop();
-                    System.out.println("Application closed");
+                    System.out.println("Web Server stopped");
                     break;
                 case "exit":
                     webServer.stop();
                     context.close();
-                    running =false;
-                    System.out.println("Web Server stopped");
-
+                    running = false;
+                    System.out.println("Application exited");
                     break;
-
+                default:
+                    System.out.println("Unknown command. Use: start | stop | exit");
             }
-
         }
 
 
-
-
-
-
+    }
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        return new TomcatServletWebServerFactory();
 
     }
 
