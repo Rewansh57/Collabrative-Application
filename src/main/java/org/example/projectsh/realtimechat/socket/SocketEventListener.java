8ecafc2a4f8dfc1ebb6e192b2968e3a858faee2d
Rewansh Choudhary <rewanshchoudhary57@gmail.com>
2025-06-12 00:24:47 +0530

    This websocket method made

--- src/main/java/org/example/projectsh/realtimechat/socket/SocketEventListener.java
+++ src/main/java/org/example/projectsh/realtimechat/socket/SocketEventListener.java
@@ -1,8 +1,10 @@
 package org.example.projectsh.realtimechat.socket;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
 public class SocketEventListener {
@@ -13,8 +15,34 @@ public class SocketEventListener {
     }
 
     @EventListener
-    public void getHeaders(SessionConnectedEvent event) {
-        StompHeaderAccessor accessor= StompHeaderAccessor.wrap()
+    public void handleClientConnectListener(SessionConnectedEvent event) {
+        StompHeaderAccessor accessor= StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId=accessor.getSessionId();
+        String username=accessor.getFirstNativeHeader("username");
+
+
+        System.out.println("User connected: "+sessionId);
+
+        if (!(username==null)){
+            webSocketConnectedRegistry.addClient(sessionId, username);
+            System.out.println("Client connected: "+sessionId);
+
+
+        }
+
+
+
+
+
+    }
+    public void handleClientDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor= StompHeaderAccessor.wrap(event.getMessage());
+       String sesId= accessor.getSessionId();
+
+       webSocketConnectedRegistry.removeClient(sesId);
+       System.out.println("Client disconnected: "+sesId);
+
+
 
     }
 }
